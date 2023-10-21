c1feae593e92c367ca0eef9d541482ceb7610419
Juan Canales <limon99jc@gmail.com>
2023-10-21 05:31:15 +0200

    sidebar added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter as FontSans } from 'next/font/google'
 import './globals.css'
 import { cn } from '@/lib/utils'
-import Navigation from '@/components/Navigation/Navigation'
+import SidebarWrapper from '@/components/SidebarWrapper'
 
 export const fontSans = FontSans({ 
   subsets: ['latin'], 
@@ -25,8 +25,8 @@ export default function RootLayout({
       className={cn(
         "min-h-screen bg-background font-sans antialiased",
         fontSans.variable
-      )}>
-        <Navigation />        
+      )}>   
+        <SidebarWrapper />
         {children}
       </body>
     </html>
